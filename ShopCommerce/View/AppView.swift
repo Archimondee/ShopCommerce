8b643f294870c95048b5a9d304d0ff0e4876957d
Gilang <gilang@62teknologi.com>
2024-02-11 16:34:23 +0700

    First Init

---
+++ ShopCommerce/View/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  ShopCommerce
+//
+//  Created by Gilang Aditya Rahman on 11/02/24.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
